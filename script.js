51e747848c2717d7b9ed13131209a1624f99de14
marina oliveira <marinaoliv4@gmail.com>
2021-01-24 16:46:53 -0300

    criando forma da cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d"); /*desenha o que vai acontecer no canvas */
 let box = 32; /*32px */
+let snake =[];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
 
 function criarBG() { /* criar background*/
       context.fillStyle = "lightgreen"; /*estilo */
       context.fillRect(0, 0, 16 * box, 16 * box); /*desenho do retangulo onde vai acontecer o jogo*/
 }
 
-criarBG()
+function criarCobrinha() {
+      for(i=0; i <snake.length; i++){
+          context.fillStyle = "green";
+          context.fillRect(snake[i].x, snake[i].y, box, box);
+      }
+
+}
+
+
+criarBG();
+criarCobrinha();
+
+/*adicionar um elemento e retirar o ultimo*/
